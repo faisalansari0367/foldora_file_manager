5a65b0f3524e4d9efc0b6c3e59b9a3986369b6e7
faisalansari0367 <61263149+faisalansari0367@users.noreply.github.com>
2021-07-12 11:34:38 +0530

    git

---
+++ lib/pages/Apps/apps.dart
@@ -0,0 +1,83 @@
+import 'dart:io';
+
+import 'package:files/utilities/MediaListItemUtils.dart';
+import 'package:files/utilities/MyColors.dart';
+import 'package:files/utilities/Utils.dart';
+import 'package:files/widgets/LeadingIcon.dart';
+import 'package:files/widgets/MediaListItem.dart';
+import 'package:files/widgets/MyAppBar.dart';
+import 'package:flutter/material.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path/path.dart' as p;
+
+import '../../sizeConfig.dart';
+
+class Apps extends StatefulWidget {
+  @override
+  _AppsState createState() => _AppsState();
+}
+
+class _AppsState extends State<Apps> {
+  var apps = [];
+  @override
+  Widget build(BuildContext context) {
+    // final provider = Provider.of<>(context);
+    return Scaffold(
+      appBar: MyAppBar(),
+      body: Container(
+        height: Responsive.height(100),
+        child: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder(
+                future: FileUtils.getAllLocalApps(),
+                builder: (context, snapshot) {
+                  final paths = snapshot.data;
+                  if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
+                  apps = paths;
+                  return ListView.builder(
+                    itemCount: paths.length,
+                    itemBuilder: (context, index) {
+                      final app = File(paths[index]);
+                      return MediaListItem(
+                        data: app,
+                        currentPath: app.path,
+                        title: p.basename(app.path),
+                        description: MediaUtils.description(app),
+                        index: index,
+                        ontap: () {},
+                        leading: LeadingIcon(data: app),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(12),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  // padding: EdgeInsets.all(),
+                  elevation: 4,
+                  primary: MyColors.teal,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    side: BorderSide(color: MyColors.teal),
+                  ),
+                  // padding: EdgeInsets.all(20),
+                  minimumSize: Size(Responsive.width(100), Responsive.height(6)),
+                ),
+                onPressed: () async {
+                  for (var app in apps) {
+                    await OpenFile.open(app);
+                  }
+                },
+                child: Text('Install All'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/BottomAnimation.dart
@@ -0,0 +1,75 @@
+import 'package:files/provider/StoragePathProvider.dart';
+import 'package:files/sizeConfig.dart';
+import 'package:files/utilities/MyColors.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+const duration = Duration(milliseconds: 500);
+
+class BottomSlideAnimation extends StatefulWidget {
+  final Widget child;
+  const BottomSlideAnimation({Key key, this.child}) : super(key: key);
+
+  // static AnimationController controller;
+  @override
+  _BottomSlideAnimationState createState() => _BottomSlideAnimationState();
+}
+
+class _BottomSlideAnimationState extends State<BottomSlideAnimation>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<Offset> slide;
+
+  @override
+  void initState() {
+    controller = AnimationController(vsync: this, duration: duration);
+    slide = Tween<Offset>(begin: Offset(0.0, 1.0), end: Offset(0.0, 0.0)).animate(controller);
+    // controller.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final myChild = Container(
+      height: Responsive.height(6),
+      color: MyColors.darkGrey,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.delete),
+            color: MyColors.appbarActionsColor,
+            splashColor: MyColors.whitish,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.share,
+              color: MyColors.appbarActionsColor,
+            ),
+          ),
+        ],
+      ),
+    );
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        final provider = Provider.of<StoragePathProvider>(context, listen: true);
+        print('bottom navbar ${provider.hasBottomNav}');
+        provider.hasBottomNav ? controller.forward() : controller.reverse();
+        return SlideTransition(
+          position: slide,
+          child: child,
+        );
+      },
+      child: myChild,
+    );
+  }
+}
