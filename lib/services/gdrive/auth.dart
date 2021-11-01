990589825b2b446602316fe9a175ddf6a47585b9
Faisal Ansari <61263149+faisalansari0367@users.noreply.github.com>
2021-11-01 10:14:38 +0530

    small fix

--- lib/services/gdrive/auth.dart
+++ lib/services/gdrive/auth.dart
@@ -1,4 +1,4 @@
-import 'package:files/pages/Drive/sign_in_screen.dart';
+import 'package:files/pages/Drive/drive_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
